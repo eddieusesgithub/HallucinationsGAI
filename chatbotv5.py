fff59fcca740791004ab930415ff08aa21789e73
eddieusesgithub <149799800+eddieusesgithub@users.noreply.github.com>
2024-04-04 22:32:25 +0100

    Update chatbotv5.py

--- chatbotv5.py
+++ chatbotv5.py
@@ -11,15 +11,15 @@ import argparse
 device = torch.device('cpu')    #since we are not training data, just the cpu is sufficient
 print(device)
 
-block_size = 64     #hyperparameters
-batch_size = 32
+block_size = 128     #hyperparameters
+batch_size = 64
 max_iters = 300
 learning_rate = 3e-4
 eval_iters = 100
 
 n_embd = 384
-n_head = 8
-n_layer = 8
+n_head = 32
+n_layer = 32
 dropout = 0.2
 
 with open("/path/to/directory/vocab.txt", 'r', encoding = 'utf-8') as f:
