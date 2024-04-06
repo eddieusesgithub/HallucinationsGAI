5cf76f0b3149714f402d2cb11061b3ff3dbe0515
eddieusesgithub <149799800+eddieusesgithub@users.noreply.github.com>
2024-04-06 18:53:25 +0100

    Update trainingiterationfinal.py

--- trainingiterationfinal.py
+++ trainingiterationfinal.py
@@ -27,8 +27,8 @@ while i<100:    #can set this to as many iterations as you want
     eval_iters = 100
 
     n_embd = 384
-    n_head = 32
-    n_layer = 32
+    n_head = 16
+    n_layer = 16
     dropout = 0.2
     
     with open("/path/to/directory/vocab.txt", 'r', encoding = 'utf-8') as f:    #I omit the directory to my personal computer
