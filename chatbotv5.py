97a6d80db4cd86516fa522f8d63859abd7d3deb8
eddieusesgithub <149799800+eddieusesgithub@users.noreply.github.com>
2024-04-06 19:33:12 +0100

    Update chatbotv5.py

--- chatbotv5.py
+++ chatbotv5.py
@@ -176,7 +176,7 @@ while True:
     prompt = input("Prompt:\n")
     context = torch.tensor(encode(prompt), dtype = torch.long, device = device)
     generated_chars = decode(m.generate(context.unsqueeze(0), max_new_tokens=150)[0].tolist())
-    print(f'Completion:\n{generated_chars}')
+    print(f'Response:\n{generated_chars}')
     
 
     
